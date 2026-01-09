db45bbdba9469c1408e0f04fd322e5ba473db309
stenarlaane <stenar.laane@voco.ee>
2026-01-09 11:14:54 +0200

    Kopeeritud faili sisu valmis kujundusest, juhend

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import './ExpenseItem.css'
+
+function ExpenseItem(props){
+
+    return (
+        <div className='expense-item'>
+            <div>Date</div>
+            <div className='expense-item__description'>
+                <h2>Title</h2>
+                <div className='expense-item__price'>Price</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem
