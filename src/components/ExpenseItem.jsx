290808bae4f0262fbab182d1a236151148252766
stenarlaane <stenar.laane@voco.ee>
2026-01-15 13:13:11 +0200

    Lisatud kujundus komponendile. Ylesanne 1

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,11 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
-    console.log(props)
-
     return (
         <div className='expense-item'>
-            <div>{props.data.date.toString()}</div>
+            <ExpenseDate date={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
