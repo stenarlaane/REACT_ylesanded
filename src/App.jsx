01e8e2ecc45a6d6e7f10d9f3a639dc3299573ec7
stenarlaane <stenar.laane@voco.ee>
2026-01-15 12:47:19 +0200

    Komponendid sisustatud andmetega, punkt 3 tehtud

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,26 @@ import ExpenseItem from './components/ExpenseItem'
 
 const App = () => {
 
-  const data = {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: "New book",
+      price: 30.99
+    },
+    {
     date: new Date(2024, 10, 12),
-    title: "New book",
-    price: 30.99
-  }
+    title: "New jeans",
+    price: 99.99
+    }
+  ]
+
 
   return (
     <div className="App">
-    <ExpenseItem data={data}/>
+    <ExpenseItem data={expenses[0]}/>
+    <ExpenseItem data={expenses[1]}/>
     </div>
   );
 }
 
-export default App
+export default App;
